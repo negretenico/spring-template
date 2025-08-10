e8af9db20ad5f17ebfd4ec0ab0824533a49949f8
negretenico <negretenico0@gmail.com>
2025-08-09 20:34:47 -0400

    Starting point template

---
+++ src/test/java/com/negretenico/changeme/ChangemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.negretenico.changeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
